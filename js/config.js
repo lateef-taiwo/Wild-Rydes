9fdafedd4571c841cf28d5aa2f5e0e8b78c9b8a8
lateef-taiwo <taiwolateef55@gmail.com>
2023-12-03 02:39:04 +0100

    modified  js/config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_xhd2fdNfI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7k5cpla9kbeg8m3n24tk0um6as', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
