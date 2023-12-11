3fb80b1cd03671ab85cedc8d28fcdd1a37a6cc38
lateef taiwo <66183213+lateef-taiwo@users.noreply.github.com>
2023-12-11 11:37:30 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 
 set -ue
 
-RepositoryName="${wild-rides-repo}"
-AwsRegion="${us-east-1}"
-CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+repository_name="${wild-rides-repo}"
+aws_region="${us-east-1}"
+CodeCommitUrl="https://git-codecommit.${aws_region}.amazonaws.com/v1/repos/${repository_name}"
 
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
